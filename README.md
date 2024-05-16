bb4f32cfa8937f158270fbc1ff6b9545661ee946
frarfrarf <francescatornaghi1104@gmail.com>
2024-05-16 08:58:16 +0200

    primo commit per il proj php - unione con db

---
+++ README.md
@@ -0,0 +1,1 @@
+# progetto-PHP-Fra-Ila
---
+++ index.php
@@ -0,0 +1,146 @@
+
+<?php
+    $conn= new mysqli ("localhost","root","","progetto città");
+    if($conn->connect_error){
+        die("<p>Connssione al server non riuscita: ".$conn->connect_error."<p/>");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Low budget travelling</title>
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+     <header class="header clearfix">
+            <a href="https://www.airbnb.it/?c=.pi0.pk182361646_2299084126&gclsrc=aw.ds&&c=.pi0.pk182361646_2299084126&ghost=true&gad_source=1&gclid=EAIaIQobChMIyuqvgMPNhAMVPZODBx3dFAplEAAYASAAEgIrDPD_BwE" class="header_logo">⌂</a>
+            <a href="" class="header_icon-bar">
+                <span></span>
+                <span></span>
+                <span></span>
+            </a>
+            <ul class="header_menu animate" >
+                <li class="header_menu_item"><a href="index.html">Homepage</a></li>
+                <li class="header_menu_item"><a href="pagine/Europa.html">Città europee</a></li>
+                <li class="header_menu_item"><a href="pagine/Italia.html">Città italiane</a></li>
+                <li class="header_menu_item"><a href="pagine/chi_siamo.html">Chi siamo</a></li>
+                
+    
+            </ul>
+            
+     </header>
+    
+
+    <section class="cover">
+        <div class="cover_filter"></div>
+        <div class="cover_caption">
+            <div class="cover_caption_copy">
+                <h1>LOW BUDGET TRAVELLING</h1>
+                <h2>10 città low cost da visitare assolutamente!</h2>
+                <a href="https://www.skyscanner.it/" class="button">voli low cost</a>
+            </div>
+        </div>
+    </section>
+
+    <section class="banner clearfix">
+        <div class="banner_image"></div>
+        <div class="banner_copy">
+            <div class="banner_copy_text">
+                <h3> I nostri obbiettivi</h3>
+                <p>Benvenuti su "LOW BUDGET TRAVELLING" - la vostra guida online per esplorare il mondo senza svuotare il portafoglio! Se siete giovani avventurieri in cerca di emozioni senza spendere una fortuna, siete nel posto giusto. Qui troverete consigli, trucchi e offerte imperdibili per pianificare viaggi low-cost che renderanno le vostre esperienze indimenticabili.
+                    La nostra missione è dimostrarvi che viaggiare non deve essere costoso per essere straordinario.
+                    Esplorate le nostre guide, scoprite modi innovativi per risparmiare su trasporti, alloggi e itinerari.
+                    Dimenticatevi delle preoccupazioni finanziarie e preparatevi a esplorare il mondo con "LOW BUDGET TRAVELLING"...</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="cards clearfix">
+        <div class="card">
+            <img class="card_image" src="https://i.pinimg.com/564x/31/e9/7e/31e97ea8ce1b68e6847fc1f096fcb926.jpg" alt="Nature">
+            <div class="card_copy">
+                <h3>Cosa vuol dire viaggiare low cost</h3>
+                <p>Viaggiare low cost è molto più di una semplice modalità di spostamento; è uno stile di vita che permette di esplorare il mondo con un budget limitato, ma con la massima soddisfazione. Questa filosofia di viaggio si basa sulla ricerca di soluzioni economiche senza compromettere l'esperienza o rinunciare alla scoperta di nuovi luoghi affascinanti. </p>
+                <p>Nel viaggiare low cost, ogni aspetto del viaggio è valutato attentamente per massimizzare il valore. Dall'alloggio alla ristorazione, dalle attività alle modalità di trasporto, ogni scelta è guidata dalla volontà di ottimizzare le risorse finanziarie disponibili. Ciò significa spesso optare per sistemazioni più semplici, esplorare opzioni di cibo locale economico e scegliere mezzi di trasporto più accessibili. </p>
+                <p>Tuttavia, viaggiare low cost non è solo una questione di risparmio finanziario; è anche una filosofia che promuove l'interazione con la cultura locale in maniera più autentica. Alloggiando in strutture meno convenzionali, mangiando nei luoghi frequentati dai residenti e utilizzando mezzi di trasporto pubblici, i viaggiatori low cost possono immergersi completamente nella vita quotidiana del luogo visitato. </p>
+                <p>La chiave del viaggiare low cost sta nell'essere flessibili e aperti a nuove esperienze. Spesso, le scoperte più sorprendenti avvengono quando si esplora al di fuori dei percorsi turistici tradizionali. Questo tipo di viaggio richiede un approccio avventuroso e la volontà di abbracciare l'imprevisto. </p>
+                <p>In definitiva, viaggiare low cost è una forma di esplorazione che si basa sulla consapevolezza finanziaria e sulla ricerca di autenticità. È la possibilità di vivere avventure uniche, incontrare persone interessanti e scoprire il mondo in modo economico ma appagante.</p>   
+            </div>  
+        </div>
+        <div class="card">
+            <img class="card_image" src="https://i.pinimg.com/564x/bb/61/df/bb61dfa2b66305bb02c431924ea13885.jpg" alt="Nature">
+            <div class="card_copy">
+                <h3>Metodi di alloggio alternativi</h3>
+                    <p>Dormire senza spendere soldi durante i viaggi può essere un'opzione allettante per chi desidera risparmiare e vivere esperienze più autentiche. Tuttavia, come evidenziato, questa scelta comporta una serie di compromessi e richiede flessibilità e adattabilità.
+                    Ecco alcune strategie comuni per pernottare gratuitamente durante i viaggi:
+                    </p> 
+                <h4>Couchsurfing</h4>
+                <p>Attraverso questa piattaforma online, i viaggiatori possono connettersi con persone disposte ad offrire il proprio divano o letto senza alcun costo. Questo permette di entrare in contatto con la cultura locale e fare nuove amicizie.
+                </p>
+                <h4>Scambio di case</h4>
+                <p>Diverse piattaforme consentono agli utenti di scambiare le proprie abitazioni per un periodo prestabilito. Questo permette di risparmiare sui costi dell'alloggio e di vivere come un abitante del luogo.
+                </p>
+                <h4>Lavoro in cambio di alloggio</h4>
+                <p>In alcune situazioni, è possibile ottenere alloggio gratuito in cambio di servizi lavorativi. Alcuni ostelli, ad esempio, offrono sistemazioni in cambio di alcune ore di lavoro al giorno.
+                </p>
+                <h4>Campeggio gratuito</h4>
+                <p>In alcune zone, è possibile accamparsi gratuitamente, rispettando le leggi locali e l'ambiente circostante.
+                </p>
+                <h4>Volontariato</h4>
+                <p>Partecipare a progetti di volontariato può offrire non solo un luogo dove dormire, ma anche l'opportunità di contribuire a una causa significativa.
+                </p>
+                <h4>Network di ospitalità locali</h4>
+                <p>Oltre a Couchsurfing, esistono altri network locali che mettono in contatto viaggiatori con residenti disposti ad offrire un alloggio gratuito.
+                    È fondamentale tenere presente che queste opzioni non sono sempre garantite e possono richiedere una dose di fortuna e adattabilità. Inoltre, è essenziale </p>
+            </div>  
+        </div>
+        <div class="card">
+            <img class="card_image" src="https://i.pinimg.com/564x/b3/b4/8d/b3b48df1f99d2d8627c246d52d3be027.jpg" alt="Nature">
+            <div class="card_copy">
+                <h3>Le nostre mete</h3>
+                <p>Le destinazioni che presentiamo sul nostro sito si discostano dall'itinerario tradizionale delle grandi città europee, ma questa caratteristica non le rende affatto meno affascinanti. Anzi, ciascuna di queste località propone un'esperienza unica e incantevole, distante dalla frenesia delle metropoli più celebri. </p>
+                <p>Scoprite la bellezza autentica di destinazioni meno frequentate, dove storia, cultura e natura si intrecciano in un mix indimenticabile. Questi luoghi, sebbene possano apparire meno celebrati, nascondono un fascino genuino e una ricchezza culturale pronta ad essere scoperta. </p>
+                <p>Lontani dalle tradizionali rotte turistiche, vi invitiamo a immergervi in esperienze più autentiche e meno affollate. Esplorate le strade di borghi incantevoli, assaporate la cucina locale nei mercati pittoreschi e lasciatevi sorprendere dalla bellezza di paesaggi naturali ancora poco sfruttati dal turismo di massa. </p>
+                <p>La nostra selezione di mete vi offre l'opportunità di allontanarvi dagli stereotipi e di abbracciare la diversità di luoghi meno noti ma altrettanto suggestivi. Siate pronti a vivere un viaggio fuori dagli schemi, dove l'autenticità si fonde con la scoperta, regalandovi esperienze indimenticabili e un nuovo sguardo sulle meraviglie del mondo.</p>   
+            </div>  
+        </div>
+        
+    </section>
+
+
+
+
+
+    <a href="https://wa.me/3345267982/?text= Ciao!" class="wa-link">
+        <img src="WhatsApp_icon.png" alt="" class="img-res">
+    </a>
+    
+    <footer class="footer">
+        <p> Contattaci tramite il numero 329 417 5783, oppure scrivi all'indirizzo ilariafracesca@gmail.com per maggiori informazioni</p>
+        <p> Copyright - Tieni Ilaria, Tornaghi Francesca</p>
+    </footer>
+        
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> 
+    
+    
+    <script>
+        $(document).ready(function(){
+    
+            $(".header_icon-bar").click(function(e){
+    
+                $(".header_menu").toggleClass('is-open');
+                e.preventDefault();
+            })
+    
+        });
+    
+    </script>  
+
+</body>
+</html>
